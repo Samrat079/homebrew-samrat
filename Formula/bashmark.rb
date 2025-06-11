97dced17b03bcc7fa10be1fbb00c46b71893dcd9
Samrat079 <samratdutta079@gmail.com>
2025-06-12 00:55:18 +0530

    bug fix

--- Formula/bashmark.rb
+++ Formula/bashmark.rb
@@ -8,7 +8,7 @@ class Bashmark < Formula
   depends_on "opencl-headers" # or whatever dependency is appropriate for OpenCL
 
   def install
-    bin.install "build/bashmark"
+    bin.install Dir["*/bin/bashmark"].first
   end
 
   test do
