35809f218bc0c2a5a22d55698d083c29edab16fc
Samrat079 <samratdutta079@gmail.com>
2025-06-11 23:54:14 +0530

    formula

--- Formula/bashmark.rb
+++ Formula/bashmark.rb
@@ -1,14 +1,14 @@
 class Bashmark < Formula
   desc "Bashmark is a cli based benchmarking utility, it can perform matrix math on cpu and gpu to test graphical performance."
   homepage "https://github.com/Samrat079/Bashmark"
-  url "https://github.com/Samrat079/Bashmark/releases/download/v1.0.1/bashmark-1.0.0.tar.gz"
-  sha256 "4ca8d794980ca4b8431c3cd7290f0e4f5c9c93757a87c647865dd09ad87def56"
+  url "https://github.com/Samrat079/Bashmark/releases/download/v1.0.1/bashmark-1.0.1.tar.gz"
+  sha256 "e3a92ed0e8849792762f6e11b7525631cb69b83ab3c6cf238e058652d703dd9e"
   license "MIT"
 
   depends_on "opencl-headers" # or whatever dependency is appropriate for OpenCL
 
   def install
-    bin.install "bashmark"
+    bin.install "bashmark-1.0.0.tar.gz"
   end
 
   test do
