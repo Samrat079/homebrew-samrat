68a43cf9b2f1d196db80722ae8ccd9199e08ed14
Samrat079 <samratdutta079@gmail.com>
2025-06-12 14:34:38 +0530

    path fix

--- Formula/bashmark.rb
+++ Formula/bashmark.rb
@@ -1,15 +1,16 @@
 class Bashmark < Formula
   desc "Bashmark is a cli based benchmarking utility, it can perform matrix math on cpu and gpu to test graphical performance."
   homepage "https://github.com/Samrat079/Bashmark"
-  url "https://github.com/Samrat079/Bashmark/releases/download/v1.0.1/bashmark-1.0.1.tar.gz"
-  sha256 "e3a92ed0e8849792762f6e11b7525631cb69b83ab3c6cf238e058652d703dd9e"
+  url "https://github.com/Samrat079/Bashmark/releases/download/v1.0.2/bashmark-1.0.2.tar.gz"
+  sha256 "1f441f24eeffaed0b018035bf3cb5938152a38059496b3922f5abc41f49e384c"
+  version "1.0.2"
   license "MIT"
 
   depends_on "opencl-headers" # or whatever dependency is appropriate for OpenCL
 
   def install
-      bin.install "bin/bashmark"
-      (share/"bashmark/assets").install Dir["assets/*"]
+    bin.install "bin/bashmark"
+    pkgshare.install "share/bashmark/assets"
   end
 
   test do
