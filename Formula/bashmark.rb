ea29b7fbc885dbec2ed819a23fe5851239abc9f7
Samrat079 <samratdutta079@gmail.com>
2025-06-12 16:31:35 +0530

    path fix

--- Formula/bashmark.rb
+++ Formula/bashmark.rb
@@ -2,7 +2,7 @@ class Bashmark < Formula
   desc "Bashmark is a cli based benchmarking utility, it can perform matrix math on cpu and gpu to test graphical performance."
   homepage "https://github.com/Samrat079/Bashmark"
   url "https://github.com/Samrat079/Bashmark/releases/download/v1.0.2/bashmark-1.0.2.tar.gz"
-  sha256 "1f441f24eeffaed0b018035bf3cb5938152a38059496b3922f5abc41f49e384c"
+  sha256 "00aa2537187124dedeec5c0a742acb5c1b1f8f7dac18ce614607d528a9dc061c"
   version "1.0.2"
   license "MIT"
 
